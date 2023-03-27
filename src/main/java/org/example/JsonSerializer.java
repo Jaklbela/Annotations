0a4a2f46b4c304005e997bc00e6432bf8b33d0a8
jaklbela <KsZh11@mail.ru>
2023-03-27 23:47:03 +0300

    Initial commit

---
+++ src/main/java/org/example/JsonSerializer.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.json.JSONObject;
+import org.reflections.ReflectionUtils;
+
+import java.lang.annotation.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+public class JsonSerializer<T> {
+    private final Set<Field> publishedFields;
+
+    public JsonSerializer(Class<T> serializedClass) {
+        publishedFields = new HashSet<Field>();
+        for (Field field : ReflectionUtils.getAllFields(serializedClass)) {
+            if (field.getAnnotation(Published.class) != null) {
+                publishedFields.add(field);
+            }
+        }
+    }
+
+    public JSONObject serialize(T o) throws IllegalAccessException {
+        JSONObject result = new JSONObject();
+        for (Field field : publishedFields) {
+            field.setAccessible(true);
+            result.put(field.getName(), field.get(o));
+        }
+        return result;
+    }
+}
